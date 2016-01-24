81dffd7c01eb65a5fb588b12cbeb18714c8d0c84
Sajin7 <sajin.sasy@gmail.com>
2016-01-24 16:23:57 -0500

    PRIMEDST
    Need optimal Solution
    Notes : 
    Converting to Adjacency Matrix (X) highly inefficient.

---
+++ PRIMEDST.c
@@ -0,0 +1,31 @@
+/*
+ Author - SDK
+ CodeChef - PRIMEDST
+ Prime Distance on Tree
+*/
+
+#include<stdio.h>
+
+int main()
+{
+
+int n;
+
+int *a,*b;
+a = new int[n];
+b = new int[n];
+
+int i;
+for(i=0;i<n;i++)
+	{
+	scanf("%d %d",&a[i],&b[i]);
+	}
+
+int total_paths = (n * (n-1))/2;
+
+// Count Prime Paths
+int prime_paths;
+
+
+
+}
